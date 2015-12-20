eee505b2dd8fd25d5ad0ab61b3ff265d58766c5c
Rob Schley <eclecticrob@gmail.com>
2015-12-20 17:54:39 -0600

    Fixed Method.options setter detection of other option setters. Added more unit tests for Method.

--- spec/MethodSpec.js
+++ spec/MethodSpec.js
@@ -32,4 +32,167 @@ describe('Method', function() {
     expect(throwImplementation).toThrowError(assert.AssertionError, 'implementation must be a function');
     expect(success).not.toThrow();
   });
+
+  describe('name', function() {
+
+    var method = new service.Types.Method('test', function() {});
+
+    it('should be gettable', function() {
+      expect(method.name).toBe('test');
+    });
+
+    it('should be settable', function() {
+      method.name = 'foo';
+      expect(method.name).toBe('foo');
+    });
+
+    it('should only allow a string', function() {
+      var setNumber = function() {
+        method.name = 1;
+      };
+      expect(setNumber).toThrowError(assert.AssertionError, 'name must be a string');
+    });
+  });
+
+  describe('implementation', function() {
+    var fnTruthy = function() { return true; };
+    var fnFalsy = function() { return true; };
+
+    var method = new service.Types.Method('test', fnTruthy);
+
+    it('should be gettable', function() {
+      expect(method.implementation).toBe(fnTruthy);
+    });
+
+    it('should be settable', function() {
+      method.implementation = fnFalsy;
+      expect(method.implementation).toBe(fnFalsy);
+    });
+
+    it('should only allow a function', function() {
+      var setNumber = function() {
+        method.implementation = 1;
+      };
+      expect(setNumber).toThrowError(assert.AssertionError, 'implementation must be a function');
+    });
+  });
+
+  describe('options', function() {
+    var optsTruthy = { foo: true };
+    var optsFalsy = { foo: false };
+
+    var method = new service.Types.Method('test', function() {}, optsTruthy);
+
+    it('should be gettable', function() {
+      expect(method.options).toEqual(optsTruthy);
+    });
+
+    it('should be settable', function() {
+      method.options = optsFalsy;
+      expect(method.options).toEqual(optsFalsy);
+    });
+
+    it('should only allow an object', function() {
+      var setNumber = function() {
+        method.options = 1;
+      };
+      expect(setNumber).toThrowError(assert.AssertionError, 'options must be an object');
+    });
+
+    it('should set method\'s own properties on method', function() {
+      var optsWithName = { name: 'renamed' };
+      method.options = optsWithName;
+      expect(method.name).toEqual('renamed');
+      expect(method.options.name).toBeUndefined(undefined);
+    });
+  });
+
+  describe('invoke', function() {
+
+    // Create an echo method.
+    var echo = new service.Types.Method(
+      'echo',
+      function(text) {
+        return text;
+      }
+    );
+
+    it('should be a function', function() {
+      expect(typeof echo.invoke).toEqual('function');
+    });
+
+    it('should return a promise', function() {
+      expect(echo.invoke('Foo') instanceof Promise).toEqual(true);
+    });
+
+    it('should fulfill a promise on success', function(done) {
+      echo
+        .invoke('Foo')
+        .then(function(res) {
+          expect(res).toEqual('Foo');
+          done();
+        })
+        .catch(function(error) {
+          done.fail('promise rejected');
+        });
+    });
+
+    it('should reject promise with NotFound error on null', function(done) {
+
+      // Create a notFound method.
+      var notFound = new service.Types.Method(
+        'notFound',
+        function() {
+          return null;
+        }
+      );
+
+      notFound
+        .invoke()
+        .then(function(res) {
+          done.fail('promise should be rejected');
+        })
+        .catch(function(error) {
+          expect(error instanceof errors.NotFound).toEqual(true);
+          done();
+        });
+    });
+
+    it('should reject a promise on error', function(done) {
+
+      // Create a reject method.
+      var reject = new service.Types.Method(
+        'reject',
+        function() {
+          throw new errors.Error('Nope');
+        }
+      );
+
+      reject
+        .invoke()
+        .then(function(res) {
+          done.fail('promise should be rejected');
+        })
+        .catch(function(error) {
+          expect(error.message).toEqual('Nope');
+          done();
+        });
+    });
+
+    it('should pass on all arguments plus itself', function(done) {
+
+      // Create a method to count it's arguments.
+      var counter = new service.Types.Method(
+        'counter',
+        function() {
+          return arguments.length;
+        }
+      );
+
+      counter.invoke(1, 2, 3).then(function(res) {
+        expect(res).toEqual(4);
+        done();
+      });
+    });
+  });
 });
